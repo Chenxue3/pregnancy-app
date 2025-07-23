07796b2531ba9887927dc85923bf836895fab127
Chenxue3 <Chenxue3@users.noreply.github.com>
2025-07-23 21:17:59 +0000

    deploy: 43b47d1dc2b6819bd28f2a23de7e6315ec621069

--- _nuxt/static/1753238113/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Ffailure-decompensated","\u002Fvideo","\u002Fmodel-heart","\u002Fattack-healthy","\u002Fattack-minor","\u002Felectricity-healthy","\u002Fattack-severe","\u002Ffailure-compensated","\u002Fmodel-heart#video-div","\u002Fabout","\u002Ffailure-healthy","\u002Felectricity-fibrillation"]})
